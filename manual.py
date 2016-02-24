bb342841ce32cc3b4922018ac56d57ef51a3c1a2
marti2dv <marti2dv@gmail.com>
2016-02-24 16:41:40 -0500

    added other pages, links between them, and CSS

--- manual.py
+++ manual.py
@@ -15,6 +15,22 @@ class MainPage(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render())
 
+
+class UserInterfacePage(webapp2.RequestHandler):
+
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('userInterface.html')
+        self.response.write(template.render())
+
+class FAQPage(webapp2.RequestHandler):
+
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('FAQ.html')
+        self.response.write(template.render())
+
+
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/UserInterface', UserInterfacePage),
+    ('/FAQ', FAQPage),
 ], debug=True)
